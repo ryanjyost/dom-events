ff5926632f4aa0d5a3161dde1ae40efea54d3101
Ryan Yost <ryost217@gmail.com>
2017-11-01 20:44:28 -0500

    refactoring, experimenting with event handling

--- scripts.js
+++ scripts.js
@@ -1,425 +1,39 @@
-// Events
-const events = [
-  {
-    eventName: "blur",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/blur",
-    active: true,
-    selectors: ["input", "select", "textarea"]
-  },
-  {
-    eventName: "change",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/change",
-    active: true,
-    selectors: ["input", "select", "textarea"]
-  },
-  {
-    eventName: "click",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/click",
-    active: true,
-    selectors: [
-      "main",
-      "h3",
-      "p",
-      "form",
-      "input",
-      "textarea",
-      "select",
-      "button",
-      "video",
-      "img"
-    ]
-  },
-  {
-    eventName: "contextmenu",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/contextmenu",
-    active: true,
-    selectors: ["main"]
-  },
-  {
-    eventName: "DOMContentLoaded",
-    link:
-      "https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded",
-    active: true,
-    selectors: ["document"]
-  },
-  {
-    eventName: "dblclick",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/dblclick",
-    active: true,
-    selectors: [
-      "main",
-      "h3",
-      "p",
-      "form",
-      "input",
-      "textarea",
-      "select",
-      "button",
-      "video",
-      "img"
-    ]
-  },
-  {
-    eventName: "focus",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/focus",
-    active: true,
-    selectors: [
-      "main",
-      "h3",
-      "p",
-      "form",
-      "input",
-      "textarea",
-      "select",
-      "button",
-      "video",
-      "img"
-    ]
-  },
-  {
-    eventName: "focusin",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/focusin",
-    active: false,
-    selectors: [
-      "main",
-      "h3",
-      "p",
-      "form",
-      "input",
-      "textarea",
-      "select",
-      "button",
-      "video",
-      "img"
-    ]
-  },
-  {
-    eventName: "focusout",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/focusout",
-    active: false,
-    selectors: [
-      "main",
-      "h3",
-      "p",
-      "form",
-      "input",
-      "textarea",
-      "select",
-      "button",
-      "video",
-      "img"
-    ]
-  },
-  {
-    eventName: "input",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/input",
-    active: true,
-    selectors: ["input", "select", "textarea"]
-  },
-  {
-    eventName: "keydown",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/keydown",
-    active: false,
-    selectors: ["input", "select", "textarea", "window"]
-  },
-  {
-    eventName: "keypress",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/keypress",
-    active: true,
-    selectors: ["input", "select", "textarea", "window"]
-  },
-  {
-    eventName: "keyup",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/keyup",
-    active: false,
-    selectors: ["input", "select", "textarea", "window"]
-  },
-  {
-    eventName: "load",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/load",
-    active: false,
-    selectors: ["video", "img"]
-  },
-  {
-    eventName: "mousedown",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/mousedown",
-    active: true,
-    selectors: ["input", "select", "textarea", "main"]
-  },
-  {
-    eventName: "mouseenter",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/mouseenter",
-    active: false,
-    selectors: [
-      "main",
-      "h3",
-      "p",
-      "form",
-      "input",
-      "textarea",
-      "select",
-      "button",
-      "video",
-      "img"
-    ]
-  },
-  {
-    eventName: "mouseleave",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/mouseleave",
-    active: false,
-    selectors: [
-      "main",
-      "h3",
-      "p",
-      "form",
-      "input",
-      "textarea",
-      "select",
-      "button",
-      "video",
-      "img"
-    ]
-  },
-  {
-    eventName: "mousemove",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/mousemove",
-    active: false,
-    selectors: [
-      "main",
-      "h3",
-      "p",
-      "form",
-      "input",
-      "textarea",
-      "select",
-      "button",
-      "video",
-      "img"
-    ]
-  },
-  {
-    eventName: "mouseout",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/mouseout",
-    active: false,
-    selectors: [
-      "main",
-      "h3",
-      "p",
-      "form",
-      "input",
-      "textarea",
-      "select",
-      "button",
-      "video",
-      "img"
-    ]
-  },
-  {
-    eventName: "mouseover",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/mouseover",
-    active: false,
-    selectors: [
-      "main",
-      "h3",
-      "p",
-      "form",
-      "input",
-      "textarea",
-      "select",
-      "button",
-      "video",
-      "img"
-    ]
-  },
-  {
-    eventName: "mouseup",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/mouseup",
-    active: true,
-    selectors: [
-      "main",
-      "h3",
-      "p",
-      "form",
-      "input",
-      "textarea",
-      "select",
-      "button",
-      "video",
-      "img"
-    ]
-  },
-  {
-    eventName: "play",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/play",
-    active: true,
-    selectors: ["video"]
-  },
-  {
-    eventName: "reset",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/reset",
-    active: true,
-    selectors: ["form"]
-  },
-  {
-    eventName: "scroll",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/scroll",
-    active: true,
-    selectors: ["main"]
-  },
-  {
-    eventName: "seeked",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/seeked",
-    active: true,
-    selectors: ["video"]
-  },
-  {
-    eventName: "seeking",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/seeking",
-    active: true,
-    selectors: ["video"]
-  },
-  {
-    eventName: "select",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/select",
-    active: true,
-    selectors: [
-      "main",
-      "h3",
-      "p",
-      "form",
-      "input",
-      "textarea",
-      "select",
-      "button",
-      "video",
-      "img"
-    ]
-  },
-  {
-    eventName: "storage",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/storage",
-    active: true,
-    selectors: ["window"]
-  },
-  {
-    eventName: "submit",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/submit",
-    active: true,
-    selectors: ["form"]
-  },
-  {
-    eventName: "timeupdate",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/timeupdate",
-    active: false,
-    selectors: ["video"]
-  },
-  {
-    eventName: "volumechange",
-    link: "https://developer.mozilla.org/en-US/docs/Web/Events/volumechange",
-    active: true,
-    selectors: ["video"]
-  }
-];
+//=== Populate localStorage with above defaults if empty ===//
+if (!localStorage.getItem("eventSettings")) {
+  const eventDefaults = defaultSettings.getEventDefaults();
+  localStorage.setItem("eventSettings", JSON.stringify(eventDefaults));
+}
 
-// Selectors
-const selectors = [
-  {
-    name: "window",
-    element: window,
-    active: true
-  },
-  {
-    name: "document",
-    element: document,
-    active: true
-  },
-  {
-    name: "main",
-    element: document.querySelector("main"),
-    active: true
-  },
-  {
-    name: "h3",
-    element: document.querySelector("h3"),
-    active: true
-  },
-  {
-    name: "p",
-    element: document.querySelector("p"),
-    active: true
-  },
+if (!localStorage.getItem("targetSettings")) {
+  const targetDefaults = defaultSettings.getTargetDefaults();
+  localStorage.setItem("targetSettings", JSON.stringify(targetDefaults));
+}
 
-  {
-    name: "form",
-    element: document.querySelector("form"),
-    active: true
-  },
-  {
-    name: "input",
-    element: document.querySelector("input"),
-    active: true
-  },
-  {
-    name: "textarea",
-    element: document.querySelector("textarea"),
-    active: true
-  },
-  {
-    name: "select",
-    element: document.querySelector("select"),
-    active: true
-  },
-  {
-    name: "button",
-    element: document.querySelector("button"),
-    active: true
-  },
-  {
-    name: "video",
-    element: document.querySelector("video"),
-    active: true
-  },
-  {
-    name: "img",
-    element: document.querySelector("img"),
-    active: true
-  }
-];
+//=========populate event/target filters with toggle buttons ==========//
+const eventOptions = getLocalEventSettings();
+const targetOptions = getLocalTargetSettings();
 
-//=========populate listener filters ==========//
-const selectorFilter = document.getElementById("selectorFilters");
-const eventFilter = document.getElementById("eventFilters");
+populateListenerFilters(eventOptions, "event");
+populateListenerFilters(targetOptions, "target");
 
-events.forEach(event => {
-  let eventToggle = document.createElement("li");
-  eventToggle.classList.add("toggleButton");
-  eventToggle.innerHTML = event.eventName;
+//====== Add listeners at initial page load if active =====//
+targetOptions.forEach(target => {
+  if (target.isActive) {
+    let eventTarget;
+    switch (target.name) {
+      case "window":
+        eventTarget = window;
+        break;
+      case "document":
+        eventTarget = document;
+        break;
+      default:
+        eventTarget = document.querySelector(target.name);
+    }
 
-  if (event.active) {
-    eventToggle.classList.add("activeFilter");
-  }
-
-  eventFilter.appendChild(eventToggle);
-});
-
-selectors.forEach(selector => {
-  let selectorToggle = document.createElement("li");
-  selectorToggle.classList.add("toggleButton");
-  selectorToggle.innerHTML = selector.name;
-
-  if (selector.active) {
-    selectorToggle.classList.add("activeFilter");
-  }
-
-  selectorFilter.appendChild(selectorToggle);
-});
-
-//====== Add active event listeners to active, appllicable selectors =====//
-selectors.forEach(selector => {
-  if (selector.active) {
-    events.forEach(event => {
-      if (event.active && event.selectors.includes(selector.name)) {
-        selector.element.addEventListener(event.eventName, handleEvent);
+    eventOptions.forEach(event => {
+      if (event.isActive) {
+        eventTarget.addEventListener(event.name, handleEvent);
       }
     });
   }
@@ -431,6 +45,10 @@ let eventLog = document.getElementById("eventLog"),
   eventRepeatCounter = 1;
 
 function handleEvent(e) {
+  //we only care about the event target, and want to avoid duplicate actions
+  //when bubbling up. Since everything has listener, bubbles to itself
+  e.stopPropagation();
+
   //prevent page reload on form submission
   if (e.target.tagName == "FORM") {
     e.preventDefault();
@@ -439,7 +57,7 @@ function handleEvent(e) {
   //log event object for user reference
   console.log(e);
 
-  //specify the label for the event target
+  //specify the label for the event target with currentTarget, so inactive targets don't show up when they are the event target
   let eventTarget = e.currentTarget,
     targetLabel = "";
 
@@ -454,7 +72,7 @@ function handleEvent(e) {
   if (prevEvent.eventType == e.type && prevEvent.targetLabel == targetLabel) {
     //get the list item most recently appended, b/c it is same as current event
     let mostRecentEventItem = document.querySelector("#eventLog > li");
-    console.log(mostRecentEventItem);
+    //console.log(mostRecentEventItem);
 
     eventRepeatCounter += 1;
     mostRecentEventItem.classList.remove("eventItemColorFlash");
@@ -504,3 +122,53 @@ function toggleFilterContainer(e) {
 document
   .getElementById("filterContainerToggle")
   .addEventListener("click", toggleFilterContainer);
+
+//========= Toggle Listeners ==========//
+const toggleButtons = document.querySelectorAll(".filterSection li");
+
+toggleButtons.forEach(button => {
+  button.addEventListener("click", toggleListener);
+});
+
+function toggleListener(e) {
+  let targetName = e.target.innerHTML;
+  let toggleOptions = [...targets, ...events];
+
+  let targetInfo = toggleOptions.find(option => {
+    return option.name === targetName;
+  });
+
+  // targetInfo.active ?
+
+  // targetInfo.active = !targetInfo.active
+}
+
+//========= HELPER FUNCTIONS ==========//
+
+function getLocalEventSettings() {
+  return JSON.parse(localStorage.getItem("eventSettings"));
+}
+
+function getLocalTargetSettings() {
+  return JSON.parse(localStorage.getItem("targetSettings"));
+}
+
+function populateListenerFilters(options, type) {
+  options.forEach(option => {
+    const optionToggleButton = document.createElement("li");
+    optionToggleButton.classList.add("toggleButton");
+    optionToggleButton.innerHTML = option.name;
+
+    if (option.isActive) {
+      optionToggleButton.classList.add("activeFilter");
+    }
+
+    type === "event"
+      ? document.getElementById("eventFilters").appendChild(optionToggleButton)
+      : document
+          .getElementById("targetFilters")
+          .appendChild(optionToggleButton);
+  });
+
+  return;
+}
